781978ce230421b6fc79808a9b26aeae856a218e
kwonyj <loling970124@gmail.com>
2021-05-19 09:17:22 +0900

    Initial commit

---
+++ src/algorithmStudy/No1000.java
@@ -0,0 +1,13 @@
+package algorithmStudy;
+import java.util.Scanner;
+
+public class No1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		System.out.println(a+b);
+	}
+
+}
