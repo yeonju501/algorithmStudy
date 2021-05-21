f0ecbbdb4fc5f6d5c4663a4f8f0e8bf66587f9f1
kwonyj <loling970124@gmail.com>
2021-05-21 13:48:21 +0900

    myPrideTorntoShreds

---
+++ src/algorithmStudy/stringtokenizer.java
@@ -0,0 +1,20 @@
+package algorithmStudy;
+
+import java.util.*;
+
+public class stringtokenizer {
+	public static void main(String[] args) {
+		String a1 = "H/E/L/L/O";
+		StringTokenizer st = new StringTokenizer(a1, "/");
+		StringBuffer sb = new StringBuffer();
+		while(st.hasMoreTokens()) {
+			sb.append(st.nextToken());
+		}
+		char a2[] = sb.toString().toCharArray();
+		System.out.print(a2);
+		String a3 = null;
+		a3 += "gkgk";
+		System.out.println(a3);
+	}
+
+}
