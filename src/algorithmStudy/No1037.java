8aa847a409928ebacf0b9a800523d260d2ad9f33
kwonyj <loling970124@gmail.com>
2021-06-23 01:07:20 +0900

    why

--- src/algorithmStudy/No1037.java
+++ src/algorithmStudy/No1037.java
@@ -1,22 +1,19 @@
 package algorithmStudy;
-import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class No1037 {
-	public static void Main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int su = Integer.parseInt(br.readLine());
-		String Astr = br.readLine();
-		int N;
-		String[] Astrarr = Astr.split(" ");
-		int[] A = new int[Astrarr.length];
-		for(int i = 0; i < Astrarr.length; i++)
-			A[i] = Integer.parseInt(Astrarr[i]);
-		if(su == 1)
-			N = A[0]^2;
-		else
-			N = A[0] * A[A.length - 1];
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] A = new int[num];
+		for(int i = 0; i < A.length; i++) {
+			A[i] = sc.nextInt();
+		}
+		Arrays.sort(A);
+		int N = A[0] * A[num - 1];
+		sc.close();
 		System.out.println(N);
-			
 	}
 
 }
